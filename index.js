a9efe7663248e20fcc06d8ca714ad6ea835aff04
OlegSenko <senkooleg@gmail.com>
2024-01-12 10:13:55 +0300

    end perfect pixel header+filters

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // types
 const types = document.querySelector(".types");
-const typesTitle = types.querySelector(".types__title");
+const typesTitle = types.querySelector(".types__title-wrapper");
+const typesTitleContent = types.querySelector(".types__title");
 const typesLabels = types.querySelectorAll(".select__label-types");
 
 typesTitle.addEventListener("click", () => {
@@ -13,30 +14,28 @@ typesTitle.addEventListener("click", () => {
 
 for (let i = 0; i < typesLabels.length; i++) {
   typesLabels[i].addEventListener("click", (evt) => {
-    typesTitle.textContent = evt.target.textContent;
+    typesTitleContent.textContent = evt.target.textContent;
     types.setAttribute("data-state", "");
   });
 }
+
 // brand
 const brand = document.querySelector(".brand");
-const brandTitle = brand.querySelector(".brand__title");
+const brandTitle = brand.querySelector(".brand__title-wrapper");
+const brandTitleContent = brand.querySelector(".brand__title");
 const brandLabels = brand.querySelectorAll(".select__label-brand");
 
 brandTitle.addEventListener("click", () => {
-  console.log(brandLabels);
   if ("active" === brand.getAttribute("data-state")) {
-    console.log(1);
     brand.setAttribute("data-state", "");
   } else {
-    console.log(2);
     brand.setAttribute("data-state", "active");
   }
 });
 
 for (let i = 0; i < brandLabels.length; i++) {
-  brandLabels[i].addEventListener("click", (evt) => {
-    console.log(evt);
-    brandTitle.textContent = evt.target.textContent;
+  brandLabels[i].addEventListener("click", (e) => {
+    brandTitleContent.textContent = e.target.textContent;
     brand.setAttribute("data-state", "");
   });
 }
